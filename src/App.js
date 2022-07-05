7febf9eed844496b06d67c27b5ce896847117e2b
مهدی روشن* <Roshan@sava.local>
2022-07-05 15:02:04 +0430

    first page design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { Route , Routes } from "react-router-dom";
+
+import ProductCard from "./pages/productCard";
 
 function App() {
-  
+  return (
+    <Routes>
+      <Route  path="/fastcart/:productId" element={<ProductCard />} />
+    </Routes>
+  );
 }
 
 export default App;
